62bb5a3058141964dea9e9e35814985bbe7874e7
Travis <travisstew214@gmail.com>
2020-06-08 22:14:05 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-require('dotenv').config();
+// require('dotenv').config();
 
 router.get('/', (req, res)=>{
     res.send('hello');
---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+
+
+
+const PORT = process.env.PORT || 3000;
+
+//static files
+app.use(express.static('public'));
+
+//template engine
+// app.engine('handlebars', exphbs());
+// app.set('view engine', 'handlebars');
+
+//body parser
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// require('dotenv').config();
+
+// app.use('/', require('../routes/index'));
+
+app.use('/.netlify/functions/app',require('../routes/index'))
+
+// app.listen(PORT,function () { 
+//   console.log('listening on port ' + PORT);
+//  });
+
+ module.exports.handler = serverless(app);
